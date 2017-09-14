5c743d7c53a4e3f4b46e21e0280ace28b7cc50e1
JuneCho23 <junecho.ucb@gmail.com>
2017-09-13 18:38:23 -0700

    CP2

---
+++ CP2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
