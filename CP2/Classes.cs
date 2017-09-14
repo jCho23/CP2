f78d5903c0d3dc39301f9aa9172e40846b1cfb8f
JuneCho23 <junecho.ucb@gmail.com>
2017-09-13 18:40:31 -0700

    Anatomy of a Class notes

---
+++ CP2/Classes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Anatomy of a Class
+//Data = Represented by fields
+//Behvior= Represented by methods / functions
+
+
+namespace CP2
+{
+    public class Classes
+    {
+        public Classes()
+        {
+        }
+    }
+}
