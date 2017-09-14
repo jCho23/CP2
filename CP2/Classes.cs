990edd47df83608cff13d74830501346d6f35963
JuneCho23 <junecho.ucb@gmail.com>
2017-09-13 18:45:03 -0700

    Object notes

--- CP2/Classes.cs
+++ CP2/Classes.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
+//Class= Building blocks of an app
+
 //Anatomy of a Class
 //Data = Represented by fields
 //Behvior= Represented by methods / functions
 
+//Object = Instance of a Class that resides in Memory
 
 namespace CP2
 {
+    
     public class Classes
     {
         public Classes()
